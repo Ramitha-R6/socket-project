dbe7aa1f1543ff02dae75026d7f1f842fd60aa7b
Ramitha R <18780ramitha@gmail.com>
2026-03-19 23:08:38 +0530

    Added gitignore

---
+++ client/client.py
@@ -0,0 +1,29 @@
+import socket
+import ssl
+
+HOST = "127.0.0.1"
+PORT = 5000
+
+context = ssl.create_default_context()
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+secure_socket = context.wrap_socket(sock, server_hostname=HOST)
+
+secure_socket.connect((HOST, PORT))
+
+file = open("received.bin", "wb")
+
+while True:
+
+    data = secure_socket.recv(1024)
+
+    if not data:
+        break
+
+    file.write(data)
+
+file.close()
+secure_socket.close()
+
+print("File download complete")
---
+++ client/client1.py
@@ -0,0 +1,29 @@
+import socket
+import ssl
+
+HOST = "10.30.200.210"
+PORT = 5000
+
+# Disable certificate verification for self-signed cert
+context = ssl._create_unverified_context()
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+secure_socket = context.wrap_socket(sock, server_hostname=HOST)
+
+secure_socket.connect((HOST, PORT))
+
+file = open("received.bin", "wb")
+
+while True:
+    data = secure_socket.recv(1024)
+
+    if not data:
+        break
+
+    file.write(data)
+
+file.close()
+secure_socket.close()
+
+print("File download complete")
